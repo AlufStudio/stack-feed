f6569d291e2fdcb985d5f010e128a7095adf20fe
Tuyen Nguyen <nntuyen92@gmail.com>
2016-04-22 23:26:36 +0700

    Add base activity

---
+++ app/src/main/java/com/tuyenmonkey/stackfeed/base/BaseActivity.java
@@ -0,0 +1,42 @@
+package com.tuyenmonkey.stackfeed.base;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Tuyen Nguyen on 4/22/16.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract int getLayoutId();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initializeInjector();
+        bindViewModel();
+        initializeActivity();
+    }
+
+    protected void initializeInjector() {}
+
+    protected abstract void bindViewModel();
+
+    public ViewDataBinding getViewDataBinding() {
+        ViewDataBinding viewDataBinding = DataBindingUtil.setContentView(this, getLayoutId());
+        ButterKnife.bind(this);
+        return viewDataBinding;
+    }
+
+    protected abstract void initializeActivity();
+
+    public void showToastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/tuyenmonkey/stackfeed/presentation/main/view/MainActivity.java
+++ app/src/main/java/com/tuyenmonkey/stackfeed/presentation/main/view/MainActivity.java
@@ -1,15 +1,25 @@
 package com.tuyenmonkey.stackfeed.presentation.main.view;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.tuyenmonkey.stackfeed.R;
+import com.tuyenmonkey.stackfeed.base.BaseActivity;
+import com.tuyenmonkey.stackfeed.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void bindViewModel() {
+        this.binding = (ActivityMainBinding)getViewDataBinding();
+    }
+
+    @Override
+    protected void initializeActivity() {
+
     }
 }
